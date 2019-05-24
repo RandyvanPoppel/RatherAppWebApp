13cd8ad3606f4b94a1997dd4c18c9f248b474f2c
Randy van Poppel <randy.vanpoppel@student.fontys.nl>
2019-05-24 15:47:04 +0200

    Added routing, global app styling and ComparisonComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ComparisonComponent } from './comparison/comparison.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComparisonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
